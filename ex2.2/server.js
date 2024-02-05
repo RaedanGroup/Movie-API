45f72406ce66f371b2ec50ca149adca54169392f
Argos Stone <peter@raedangroup.com>
2024-02-05 14:01:58 -0500

    fixed broken links
    Repaired the broken html links in server.js after having moved it to a subfolder according to next lesson instructions

--- ex2.2/server.js
+++ ex2.2/server.js
@@ -7,7 +7,7 @@ http.createServer((request, response) => {
     q = new URL(addr, 'http://' + request.headers.host),
     filePath = '';
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+  fs.appendFile('../log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
     if (err) {
       console.log(err);
     } else {
@@ -16,9 +16,9 @@ http.createServer((request, response) => {
   });
 
   if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
+    filePath = ('../documentation.html');
   } else {
-    filePath = 'index.html';
+    filePath = '../index.html';
   }
 
   fs.readFile(filePath, (err, data) => {
