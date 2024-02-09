dec73cd5883f0edfd8e2fc79378d465c51f4261b
Argos Stone <peter@raedangroup.com>
2024-02-09 02:35:17 -0500

    Ex2.8 final sub
    mongo db and mongoose
    models.js created
    index.js updated
    documentation.html updated

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // set requires and constants
-const express = require('express'),
-  app = express();
+const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
+
+// mongoose and models.js require
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movie = Models.Movie;
+const User = Models.User;
+
+// mongoose connection
+mongoose.connect('mongodb://localhost:27017/movieAPI')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
 
 // use body-parser
 app.use(bodyParser.json());
@@ -11,187 +23,182 @@ app.use(bodyParser.json());
 // log all requests
 app.use(morgan('common'));
 
-// static array of 2 demo users
-let users = [
-  {
-    id: 1,
-    name: 'Argos Stone',
-    email: 'argos.stone@email.com',
-    favoriteMovies: []
-  },
-  {
-    id: 2,
-    name: 'Phaelan Mace',
-    email: 'phaelan.mace@email.com',
-    favoriteMovies: ["The Third Man"]
-  },
-]
-
-// json object of 3 demo movies
-let movies = [
-  {
-    "Title": 'The Third Man',
-    "Year": 1949,
-    "Description": 'Pulp novelist Holly Martins travels to shadowy, postwar Vienna, only to find himself investigating the mysterious death of an old friend, Harry Lime.',
-    "Genre": {
-      "Name": "Film Noir",
-      "Description": "Film noir is a cinematic term used primarily to describe stylish Hollywood crime dramas, particularly those that emphasize cynical attitudes and nebulous scenarios."
-    },
-    "Director": {
-      "Name": "Carol Reed",
-      "Bio": "Sir Carol Reed was an English film director, producer and screenwriter. He was knighted in 1952 for his contribution to the film industry.",
-      "Birth": 1906,
-      "Death": 1976
-    },
-    "ImagePath": "https://upload.wikimedia.org/wikipedia/en/5/50/ThirdManUSPoster.jpg",
-  },
-  {
-    "Title": 'Chinatown',
-    "Year": 1974,
-    "Description": 'A private detective hired to expose an adulterer finds himself caught up in a web of deceit and corruption.',
-    "Genre": {
-      "Name": "Mystery",
-      "Description": "Mystery films are a genre of film that revolves around the solution of a problem or a crime."
-    },
-    "Director": {
-      "Name": "Roman Polanski",
-      "Bio": "Roman Polanski is a Polish-French film director, producer, writer, and actor.",
-      "Birth": 1933,
-      "Death": "N/A"
-    },
-    "ImagePath": "https://upload.wikimedia.org/wikipedia/en/3/38/Chinatown_%281974_poster%29.jpg",
-  },
-  {
-    "Title": 'Blade Runner',
-    "Year": 1982,
-    "Description": 'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.',
-    "Genre": {
-      "Name": "Sci-Fi",
-      "Description": "Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction that typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life."
-    },
-    "Director": {
-      "Name": "Ridley Scott",
-      "Bio": "Sir Ridley Scott is an English film director and producer.",
-      "Birth": 1937,
-      "Death": "N/A"
-    },
-    "ImagePath": "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
-  }
-]
-
 // CREATE endpoints
 // POST new user
 app.post('/users', (req, res) => {
-  const newUser = req.body;
+  const newUser = new User(req.body);
 
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('Name is required.');
-  }
+  newUser.save()
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(400).send(err.message));
 });
 
 // UPDATE endpoints
 // PUT user info update
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
+app.put('/users/:username', (req, res) => {
+  const { username } = req.params;
   const updatedUser = req.body;
 
-  let user = users.find(user => user.id == id);
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('User not found.');
+      }
 
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('User not found.');
-  }
+      user.username = updatedUser.username;
+
+      return user.save();
+    })
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
+
 
 // PUT add movie to user's favorites
-app.put('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+app.put('/users/:username/favorite/:title', (req, res) => {
+  const { username, title } = req.params;
 
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} added to ${user.name}'s favorites.`);
-  } else {
-    res.status(400).send('User not found.');
-  }
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('User not found.');
+      }
+      Movie.findOne({ Title: title })
+        .then(movie => {
+          if (!movie) {
+            return res.status(404).send('Movie not found.');
+          }
+          user.favoriteMovies.push(movie._id);
+          return user.save();
+        })
+        .then(() => {
+          res.status(200).send(`${title} has been added to ${username}'s favorites.`);
+        })
+        .catch(err => res.status(400).send(err.message));
+    })
+    .catch(err => res.status(400).send(err.message));
 });
+
 
 // DELETE endpoints
 // DELETE movie from user's favorites
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+app.delete('/users/:username/favorite/:title', (req, res) => {
+  const { username, title } = req.params;
 
-  let user = users.find(user => user.id == id);
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('User not found.');
+      }
 
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} removed from ${user.name}'s favorites.`);
-  } else {
-    res.status(400).send('User not found.');
-  }
+      // Find the movie by title
+      Movie.findOne({ Title: title })
+        .then(movie => {
+          if (!movie) {
+            return res.status(404).send('Movie not found.');
+          }
+
+          // Find the index of the movie _id in the user's favoriteMovies array
+          const index = user.favoriteMovies.indexOf(movie._id);
+
+          // If the movie is not in the user's favorites, return 404
+          if (index === -1) {
+            return res.status(404).send(`${title} is not in ${username}'s favorites.`);
+          }
+
+          // Remove the movie _id from the user's favoriteMovies array
+          user.favoriteMovies.splice(index, 1);
+
+          // Save the updated user document
+          return user.save();
+        })
+        .then(() => {
+          // Respond with success message
+          res.status(200).send(`${title} has been removed from ${username}'s favorites.`);
+        })
+        .catch(err => res.status(400).send(err.message));
+    })
+    .catch(err => res.status(400).send(err.message));
 });
+
+
+
 
 // DELETE user
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
+app.delete('/users/:username', (req, res) => {
+  const { username } = req.params;
 
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    users = users.filter(user => user.id != id);
-    res.status(200).send(`${user.email} deleted.`);
-  } else {
-    res.status(400).send('User not found.');
-  }
+  User.findOneAndDelete({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send('User not found.');
+      }
+      res.status(200).send(`Account for ${user.email} has been deleted.`);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
+
 
 // READ endpoints
 // GET all movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movie.find()
+    .then(movies => res.status(200).json(movies))
+    .catch(err => res.status(400).send(err.message));
+});
+
+// GET all users
+app.get('/users', (req, res) => {
+  User.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(400).send(err.message));
 });
 
 // GET data about a single movie by title
 app.get('/movies/:title', (req, res) => {
   const { title } = req.params;
-  const movie = movies.find(movie => movie.Title === title);
 
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('Movie not found.');
-  }
+  Movie.findOne({ Title: title })
+    .then(movie => {
+      if (!movie) {
+        return res.status(404).send('Movie not found.');
+      }
+      res.status(200).json(movie);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
 
 // GET genre data by name
 app.get('/movies/genres/:genreName', (req, res) => {
   const { genreName } = req.params;
-  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
 
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('Genre not found.');
-  }
+  Movie.findOne({ 'Genre.Name': genreName })
+    .select('Genre') // Only select the 'Genre' field
+    .then(genre => {
+      if (!genre) {
+        return res.status(404).send('Genre not found.');
+      }
+      res.status(200).json(genre);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
+
 
 // GET director data by name
 app.get('/movies/directors/:directorName', (req, res) => {
   const { directorName } = req.params;
-  const director = movies.find(movie => movie.Director.Name === directorName).Director;
 
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('Director not found.');
-  }
+  Movie.findOne({ 'Director.Name': directorName })
+    .select('Director') // Only select the 'Director' field
+    .then(director => {
+      if (!director) {
+        return res.status(404).send('Director not found.');
+      }
+      res.status(200).json(director);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
 
 
---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: Number,
+        Death: Number
+    },
+    ImagePath: String,
+    Featured: Boolean
+});
+  
+let userSchema = mongoose.Schema({
+    id: {type: Number, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthday: Date,
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movie' }]
+});
+  
+let Movie = mongoose.model('movie', movieSchema);
+let User = mongoose.model('user', userSchema);
+  
+module.exports.Movie = Movie;
+module.exports.User = User;
